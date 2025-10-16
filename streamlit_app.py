3a8682764c28cd4d6fe339233adf410fbdaf32bb
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-10-16 11:39:35 -0300

    Remove height parameter from st_folium call

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,4 @@ folium.LayerControl().add_to(m)
 
 # Display the map
 # call to render Folium map in Streamlit
-st_data = st_folium(m, height = 700, returned_objects=[])
+st_data = st_folium(m, returned_objects=[])
