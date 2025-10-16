e8522a23fb212ab807d883125f5a857681908926
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-10-16 11:18:35 -0300

    Set Folium map width to 1000 pixels

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,4 @@ folium.LayerControl().add_to(m)
 
 # Display the map
 # call to render Folium map in Streamlit
-st_data = st_folium(m, returned_objects=[])
+st_data = st_folium(m, width = 1000, returned_objects=[])
